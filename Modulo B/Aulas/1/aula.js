de215a6d8cc7a245d99d2ee585bd93fdf53309b1
NedcloarBR <nedcloar1@hotmail.com>
2023-12-26 13:27:45 -0300

    chore: initial commit

---
+++ Modulo B/Aulas/1/aula.js
@@ -0,0 +1,40 @@
+//var a1 = carro1;
+//       |__> Recebe
+
+/** Identificadores 
+ * Podem começar com letras, $ ou _
+ * Não podem começar com números
+ * É possível usar letras e números
+ * É possível usar acentos e símbolos
+ * Não pode conter espaços
+ * Não pode usar palavras reservadas
+**/
+
+/** Dicas para Identificadores
+ * 'MAIÚSCULAS' e 'miniusculas' fazem diferença no nome da variavel
+ * Tente escolher 'nomes coerentes' para as variaveis
+ * Evite se tornar um 'Programador alfabero' o um 'Programador contador'
+**/
+
+//$ typeof
+
+//! number       ///////////// Infinity NaN
+/** 5 18 -12 inteiros
+ * 0.5 -15.9 3.14 8.0 reais
+**/
+
+//@ string
+/** 
+ * "Nome"
+ * 'Idade'
+ * `CPF`
+**/
+
+//# boolean
+/** 
+ * true
+ * false
+**/
+
+//% null, undefined, object [Array], function
+//% null é considerado um object?????
---
+++ Readme.md
@@ -0,0 +1,4 @@
+Este repositório abriga o conteúdo das aulas do [Curso de Javascript [40 Horas]](https://www.cursoemvideo.com/curso/javascript/) do Site [Curso em Video](https://www.cursoemvideo.com/) do Professor Gustavo Guanabara
+
+Fiz essas aulas no final de 2021
+Estado: Incompleto
